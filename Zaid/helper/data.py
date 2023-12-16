e721a2a2f18f3fa7fbdafdc2b815ceb6c9b56534
|•| Tʜᴇ Sʜɪᴠ Oғғɪᴄɪᴀɪʟ ⋟ <126413724+WASTERTSO@users.noreply.github.com>
2023-12-16 16:26:59 +0530

    Update data.py

--- Zaid/helper/data.py
+++ Zaid/helper/data.py
@@ -4,9 +4,9 @@ class Data:
 
     text_help_menu = (
         "**Command List & Help**\n**— Prefixes:** `.`"
-        .replace(",", "")
-        .replace("[", "")
-        .replace("]", "")
-        .replace("'", "")
+        .replace("!", "")
+        .replace("?", "")
+        .replace("+", "")
+        .replace(".", "")
     )
     reopen = [[InlineKeyboardButton("Re-Open", callback_data="reopen")]]
